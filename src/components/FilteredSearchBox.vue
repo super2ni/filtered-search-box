4f1b4feba6e2bafc19f68fe68c417e92867c4fe9
Denis Wong <denis.wong@keyprod.com>
2021-11-19 22:28:10 +0100

    f

---
+++ src/components/FilteredSearchBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="filtered-search-box">
+    <input type="text" :value="value" @input="$emit($event)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilteredSearchBox',
+  props: {
+    value: {
+      type:String,
+      required:false,
+      default(){
+        return true;
+      }
+    }
+  }
+}
+</script>
